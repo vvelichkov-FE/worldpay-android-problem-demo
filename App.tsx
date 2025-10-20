a9a8aa5ea3b577f89db8a82bf896c7e53a85c67d
Vladislav Velichkov <vvelichkov@Vladislavs-MacBook-Pro.local>
2025-10-20 11:34:36 +0300

    Worldpay input problem on android

--- App.tsx
+++ App.tsx
@@ -5,41 +5,21 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { AccessCheckoutTextInput } from '@worldpay/access-worldpay-checkout-react-native-sdk';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <Text>Android problem</Text>
+      <View>
+        <AccessCheckoutTextInput nativeID="cvcInput" placeholder="CVC" />
+      </View>
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
